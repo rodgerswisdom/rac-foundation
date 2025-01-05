89a27d0a5a949516f8d0938fff8c5fa0114a6267
RobinJames25 <jamesrobin12432@gmail.com>
2025-01-05 10:54:51 +0300

    updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
+import logo from "../../assets/21751412_6525428.svg"
 import "./Navbar.css"
+
 function Navbar() {
     return(
         <nav className="navbar">
             <div className="nav-container">
+            <img src={logo} alt="Logo" className="nav-logo" />
                 <ul className="nav-links">
                     <li>Home</li>
                     <li>About Us</li>
