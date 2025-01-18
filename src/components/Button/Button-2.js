b420b51d26fdc61cdcf30e90b019126d3f93fc01
rodgerswisdom <trulyhawona@gmail.com>
2025-01-18 17:48:44 +0300

    Fix: LEarn more button

--- src/components/Button/Button-2.js
+++ src/components/Button/Button-2.js
@@ -2,7 +2,9 @@ import "./Button-2.css";
 
 function Button2() {
     return(
+        <a href="https://rac-foundation.org/about-us">
         <button className="learn-more additional">Learn More</button>
+        </a>
     )
 }
 
